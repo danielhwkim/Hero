b7ebae6df0059ad552160f7e6a95b99002550886
danielhwkim <hoewon.kim@gmail.com>
2022-02-04 00:33:09 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ setup(
   description = 'Hero',
   author = 'Hoewon Kim',
   author_email = 'hoewon.kim@gmail.com',
-  url = 'https://github.com/danielhwkim/Hero',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/danielhwkim/Hero/archive/refs/tags/v_0_1.tar.gz',    # I explain this later on
-  keywords = ['hero'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  url = 'https://github.com/danielhwkim/Hero',
+  download_url = 'https://github.com/danielhwkim/Hero/archive/refs/tags/v_0_1.tar.gz',
+  keywords = ['hero'],
+  install_requires=[
           'argparse',
           'logging',
           'zeroconf',
@@ -20,11 +20,11 @@ setup(
           'socket'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
